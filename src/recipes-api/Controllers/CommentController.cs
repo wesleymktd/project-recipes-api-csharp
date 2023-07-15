ad43691afcaeabc1277ad5c6eb478660104404cf
wesleymktd <wesleymktd@gmail.com>
2023-07-15 09:33:38 -0300

    rota POST /commend implementado req10

--- src/recipes-api/Controllers/CommentController.cs
+++ src/recipes-api/Controllers/CommentController.cs
@@ -24,7 +24,9 @@ public class CommentController : ControllerBase
     [HttpPost]
     public IActionResult Create([FromBody]Comment comment)
     {
-        throw new NotImplementedException();
+        _service.AddComment(comment);
+        
+        return CreatedAtAction(nameof(Get), new { name = comment.Email}, comment);
     }
 
     // 11 - Sua aplicação deve ter o endpoint GET /comment/:recipeName
