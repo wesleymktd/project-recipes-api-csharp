c4b165854cfea164c07d085f79bec441746a7409
wesleymktd <wesleymktd@gmail.com>
2023-07-15 09:11:53 -0300

    endpoint DEL /user/:email implementado req9

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -62,6 +62,8 @@ public class UserController : ControllerBase
     [HttpDelete("{email}")]
     public IActionResult Delete(string email)
     {
-        throw new NotImplementedException();
+        if (!_service.UserExists(email)) return NotFound();
+        _service.DeleteUser(email);
+        return NoContent();
     } 
 }
